49bac2a3b39e8e3cdb7e03e101b4df4c1b53c065
Joshua27 <lopezmayorga27@gmail.com>
2025-09-25 21:28:44 -0600

    Comitfinal

--- Portal Academico INAM/Login.js
+++ Portal Academico INAM/Login.js
@@ -1,46 +1,49 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+   navToggle = document.getElementById('nav-toggle'),
+   navClose = document.getElementById('nav-close')
 
 /* Menu show */
-navToggle.addEventListener('click', () =>{
+navToggle.addEventListener('click', () => {
    navMenu.classList.add('show-menu')
 })
 
 /* Menu hidden */
-navClose.addEventListener('click', () =>{
+navClose.addEventListener('click', () => {
    navMenu.classList.remove('show-menu')
 })
 
 /*=============== SEARCH ===============*/
 const search = document.getElementById('search'),
-      searchBtn = document.getElementById('search-btn'),
-      searchClose = document.getElementById('search-close')
+   searchBtn = document.getElementById('search-btn'),
+   searchClose = document.getElementById('search-close')
 
 /* Search show */
-searchBtn.addEventListener('click', () =>{
+searchBtn.addEventListener('click', () => {
    search.classList.add('show-search')
 })
 
 /* Search hidden */
-searchClose.addEventListener('click', () =>{
+searchClose.addEventListener('click', () => {
    search.classList.remove('show-search')
 })
 
 /*=============== LOGIN ===============*/
 const login = document.getElementById('login'),
-      loginBtn = document.getElementById('login-btn'),
-      loginClose = document.getElementById('login-close')
+   loginBtn = document.getElementById('login-btn'),
+   loginClose = document.getElementById('login-close')
 
 /* Login show */
-loginBtn.addEventListener('click', () =>{
+loginBtn.addEventListener('click', () => {
    login.classList.add('show-login')
 })
 
 /* Login hidden */
-loginClose.addEventListener('click', () =>{
+loginClose.addEventListener('click', () => {
    login.classList.remove('show-login')
 })
 
-
+const accessBtn = document.getElementById('access-btn');
+accessBtn.addEventListener('click', () => {
+   window.location.href = './Index.html';
+});
--- Portal Academico INAM/js/Creados/Catalagos/IndexEstudiante.js
+++ Portal Academico INAM/js/Creados/Catalagos/IndexEstudiante.js
@@ -73,7 +73,7 @@
 //         });
 // });
 
-
+var Url = 'http://127.0.0.1:8000/';
 
 document.addEventListener('DOMContentLoaded', function () {
     // 1. Seleccionar el tbody de la tabla
